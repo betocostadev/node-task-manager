4f391f3099485b2bda3297b2ca5453828e8c89ca
betocostadev <robertomarx@gmail.com>
2019-07-11 10:54:57 -0300

    messing with mongo

--- mongodb.js
+++ mongodb.js
@@ -25,60 +25,9 @@ MongoClient.connect(connectionUrl, { useNewUrlParser:true }, (error,client) => {
   // Creating a const to make it easier to manipulate the DB.
   const db = client.db(databaseName)
 
-  // DB Operations CREATE:
-  // Create a collection (like creating a table on SQL dbs) and already insert one.
-  // Insert One
-  /* db.collection('users').insertOne({
-    name: 'Beto',
-    age: 33
-    // Below we will use a callback with our code to know if it worked or not.
-  }, (error, result) => {
-    if (error) {
-      return console.log(`Unable to insert user`)
-    }
-    console.log(result.ops)
-  }) */
-
-  // Insert One - BUT - Now using the ID we created above
-  /* db.collection('users').insertOne({
-    // Since ID is auto generated, we don't need to provide one.
-    // We are just providing it to check the one we created above.
-    _id: id,
-    name: 'James',
-    age: 36
-  }, (error, result) => {
-    if (error) {
-      return console.log(`Unable to insert user`)
-    }
-    console.log(result.ops)
-  }) */
-
-  // Insert Many
-  /* db.collection('users').insertMany([
-    { name: 'Betão', age: 64},
-    { name: 'Adriene', age: 65}
-  ], (error, result) => {
-    if (error) {
-      return console.log(`Could not insert many`)
-    }
-    console.log(result.ops)
-  }) */
-
-  // Insert Many - Again - Challenge
-  /* db.collection('tasks').insertMany([
-    {description: 'Gym', completed: false},
-    {description: 'Codenation', completed: true},
-    {description: 'Khan Math', completed: false}
-  ],(error, result) => {
-    if (error) {
-      return console.log(`Failed to execute: Insert Many operation.`)
-    }
-    console.log(result.ops, result.insertedCount)
-  }) */
-
   // DB Operations READ:
 
-  db.collection('users').findOne({name: 'John'}, (error, user) => {
+  db.collection('users').findOne({name: 'Regina'}, (error, user) => {
     if (error) {
       return console.log(`Unable to fetch data.`)
       // If doesn't find a match, it will return null. Just providing a better log below;
@@ -110,32 +59,45 @@ MongoClient.connect(connectionUrl, { useNewUrlParser:true }, (error,client) => {
 
   // DB Operations UPDATE:
   // Update One
-  // const updatePromise = db.collection('users').updateOne({
-  //   _id: new ObjectID("5d23b0170934d6277439699f")
-  // }, {
-  //   $set: {
-  //     name: 'Joilson',
-  //     age: 29
-  //   }
-  // })
+  /* const updatePromise = db.collection('users').updateOne({
+    _id: new ObjectID("5d23b0170934d6277439699f")
+  }, {
+    $set: {
+      name: 'Joilson',
+      age: 29
+    }
+  })
 
-  // updatePromise.then((result) => {
-  //   console.log(result)
+  updatePromise.then((result) => {
+    console.log(result)
+  }).catch((error) => {
+    console.log(error)
+  }) */
+
+  // The code below will do the same as the Code above, but it's a simpler and more used pattern.
+  // db.collection('users').updateOne({
+  //   _id: new ObjectID('5d23b0170934d6277439699f')
+  // }, {
+  //   // $set is a mongodb pattern, like, inc and many others.
+  //   $set: { age: 40 }
+  // }).then((result) => {
+  //   console.log(`Matched: ${result.matchedCount}. Modified: ${result.modifiedCount}`)
   // }).catch((error) => {
   //   console.log(error)
   // })
 
-  // The code below will do the same as the Code above, put is simpler and a more used pattern.
-  db.collection('users').updateOne({
-    _id: new ObjectID('5d23b0170934d6277439699f')
-  }, {
-    $set: { name: 'Cornholio'}
-  }).then((result) => {
-    console.log(`Matched: ${result.matchedCount}. Modified: ${result.modifiedCount}`)
-  }).catch((error) => {
-    console.log(error)
-  })
+  // UPDATE MANY
+  // db.collection('tasks').updateMany({
+  //   completed: true
+  // }, {
+  //   $set: { completed: false }
+  // }).then((result) => {
+  //   console.log(`Matched: ${result.matchedCount}. Modified: ${result.modifiedCount}`)
+  // }).catch((err) => {
+  //   console.log(err)
+  // })
 
+  // DB Operations DELETE:
   // Delete One
   /* db.collection('users').deleteOne(
     {name: 'Betão'}
@@ -146,15 +108,33 @@ MongoClient.connect(connectionUrl, { useNewUrlParser:true }, (error,client) => {
       console.log(result.deletedCount)
   }) */
 
-  // Delete Many (Uses a filter, like age>50, weight == 30, etc)
-  /* db.collection('users').deleteMany({ 50},
-    (error, result) => {
-      if (error) {
-        return console.log(error)
-      }
-      console.log(result.deletedCount)
-    })
-     */
+  // Delete One using the ObjectID
+  // db.collection('tasks').deleteOne({
+  //   _id: new ObjectID('5d23ab82a0123024006881b7')
+  // }).then((result) => {
+  //   console.log(`Deleted ${result.deletedCount} document.`)
+  // }).catch((err) => {
+  //   console.log(err)
+  // })
+
+  // Delete Many
+  // db.collection('users').deleteMany({
+  //   age: 43
+  // }).then((result) => {
+  //   console.log(`Documents deleted: ${result.deletedCount}`)
+  // }).catch((err) => {
+  //   console.log(err)
+  // })
+
+  // Delete Many, using more than one filter
+  // db.collection('users').deleteMany({
+  //   age: 42,
+  //   age: 43
+  // }).then((result) => {
+  //   console.log(`Documents deleted: ${result.deletedCount}`)
+  // }).catch((err) => {
+  //   console.log(err)
+  // })
 
 })
 
