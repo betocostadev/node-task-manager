6891bc62a7c0d199a9544271cf7a813b3d645ed5
betocostadev <robertomarx@gmail.com>
2019-07-15 19:22:25 -0300

    added some tests for promises

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 // We will use Mongoose to connect with MongoDB
 const mongoose = require('mongoose')
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api' // /taks-manager-api = db
-mongoose.connect(connectionURL, {useNewUrlParser: true}, {useCreateIndex: true})
+mongoose.connect(connectionURL, {useNewUrlParser: true}, {useCreateIndex: true}, {useFindAndModify: false})
 
 
 /* const addTask = new Task({ description: '  Use the other methods of Mongoose '})
